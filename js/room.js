0261900961dccc106873f433a73de358adf27469
SarfarajMolla23 <sarfarajmollah49@gmail.com>
2024-10-12 22:49:09 +0530

    Add user feed and camera quality

--- js/room.js
+++ js/room.js
@@ -1,19 +1,19 @@
-let messagesContainer = document.getElementById('messages');
+let messagesContainer = document.getElementById("messages");
 messagesContainer.scrollTop = messagesContainer.scrollHeight;
 
-const memberContainer = document.getElementById('members__container');
-const memberButton = document.getElementById('members__button');
+const memberContainer = document.getElementById("members__container");
+const memberButton = document.getElementById("members__button");
 
-const chatContainer = document.getElementById('messages__container');
-const chatButton = document.getElementById('chat__button');
+const chatContainer = document.getElementById("messages__container");
+const chatButton = document.getElementById("chat__button");
 
 let activeMemberContainer = false;
 
-memberButton.addEventListener('click', () => {
+memberButton.addEventListener("click", () => {
   if (activeMemberContainer) {
-    memberContainer.style.display = 'none';
+    memberContainer.style.display = "none";
   } else {
-    memberContainer.style.display = 'block';
+    memberContainer.style.display = "block";
   }
 
   activeMemberContainer = !activeMemberContainer;
@@ -21,12 +21,55 @@ memberButton.addEventListener('click', () => {
 
 let activeChatContainer = false;
 
-chatButton.addEventListener('click', () => {
+chatButton.addEventListener("click", () => {
   if (activeChatContainer) {
-    chatContainer.style.display = 'none';
+    chatContainer.style.display = "none";
   } else {
-    chatContainer.style.display = 'block';
+    chatContainer.style.display = "block";
   }
 
   activeChatContainer = !activeChatContainer;
 });
+
+let displayFrame = document.getElementById("stream__box");
+let videoFrames = document.getElementsByClassName("video__container");
+
+let userIdInDisplayFrame = null;
+
+let expandVideoFrame = (e) => {
+  let child = displayFrame.children[0];
+  if (child) {
+    document.getElementById("streams__container").appendChild(child);
+  }
+
+  displayFrame.style.display = "block";
+
+  displayFrame.appendChild(e.currentTarget);
+  userIdInDisplayFrame = e.currentTarget.id;
+
+  for (let i = 0; videoFrames.length > i; i++) {
+    if (videoFrames[i].id !== userIdInDisplayFrame) {
+      videoFrames[i].style.height = "100px";
+      videoFrames[i].style.width = "100px";
+    }
+  }
+};
+
+for (let i = 0; videoFrames.length > i; i++) {
+  videoFrames[i].addEventListener("click", expandVideoFrame);
+}
+
+let hideDisplayFrame = () => {
+  userIdInDisplayFrame = null;
+  displayFrame.style.display = null;
+
+  let child = displayFrame.children[0];
+  document.getElementById("streams__container").appendChild(child);
+
+  for (let i = 0; videoFrames.length > i; i++) {
+    videoFrames[i].style.height = "300px";
+    videoFrames[i].style.width = "300px";
+  }
+};
+
+displayFrame.addEventListener("click", hideDisplayFrame);
---
+++ js/room_rtc.js
@@ -0,0 +1,109 @@
+const APP_ID = "b019ff59defb4f24a96d9e05656daba9";
+
+let uid = sessionStorage.getItem("uid");
+if (!uid) {
+  uid = String(Math.floor(Math.random() * 10000));
+  sessionStorage.setItem("uid", uid);
+}
+
+let token = null;
+
+let client;
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let roomId = urlParams.get("room");
+
+if (!roomId) {
+  roomId = "main";
+}
+
+let localTracks = [];
+let remoteUsers = {};
+
+let joinRoomInit = async () => {
+  client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+  await client.join(APP_ID, roomId, token, uid);
+
+  client.on("user-published", handleUserPublished);
+  client.on("user-left", handleUserLeft);
+
+  joinStream();
+};
+
+let joinStream = async () => {
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks({}, {encoderConfig:{
+    width:{min:640, ideal: 1080, max:1080},
+    height:{min:480, ideal:720, max: 720}
+  }});
+
+  let player = ` <div class="video__container" id="user-container-${uid}">
+                     <div class="video-player" id="user-${uid}"></div>
+                </div>`;
+
+  document
+    .getElementById("streams__container")
+    .insertAdjacentHTML("beforeend", player);
+
+  document
+    .getElementById(`user-container-${uid}`)
+    .addEventListener("click", expandVideoFrame);
+
+  localTracks[1].play(`user-${uid}`);
+  await client.publish([localTracks[0], localTracks[1]]);
+};
+
+let handleUserPublished = async (user, mediaType) => {
+  remoteUsers[user.uid] = user;
+
+  await client.subscribe(user, mediaType);
+
+  let player = document.getElementById(`user-container-${user.uid}`);
+
+  if (player === null) {
+    player = `<div class="video__container" id="user-container-${user.uid}">
+    <div class="video-player" id="user-${user.uid}"></div>
+</div>`;
+
+    document
+      .getElementById("streams__container")
+      .insertAdjacentHTML("beforeend", player);
+
+    document
+      .getElementById(`user-container-${user.uid}`)
+      .addEventListener("click", expandVideoFrame);
+  }
+
+  if (displayFrame.style.display) {
+    player.style.height = "100px";
+    player.style.width = "100px";
+  }
+
+  if (mediaType === "video") {
+    user.videoTrack.play(`user-${user.uid}`);
+  }
+
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+};
+
+let handleUserLeft = async (user) => {
+  delete remoteUsers[user.uid];
+  document.getElementById(`user-container-${user.uid}`).remove();
+
+  if (userIdInDisplayFrame === `user-container-${user.uid}`) {
+    displayFrame.style.display = null;
+
+    let videoFrames = document.getElementsByClassName("video__container");
+
+    for (let i = 0; videoFrames.length > i; i++) {
+      videoFrames[i].style.height = "300px";
+      videoFrames[i].style.width = "300px";
+    }
+  }
+};
+
+
+
+joinRoomInit();
