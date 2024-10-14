1d1cfa9c0060d27df7712360fc2b87047a26ad73
SarfarajMolla23 <sarfarajmollah49@gmail.com>
2024-10-14 11:36:49 +0530

    fix issue

--- js/room_rtm.js
+++ js/room_rtm.js
@@ -111,7 +111,7 @@ let addBotMessageToDom = (botMessage) => {
 
   let newMessage = `<div class="message__wrapper">
                       <div class="message__body__bot">
-                          <strong class="message__author__bot">🤖 Mumble Bot</strong>
+                          <strong class="message__author__bot">🤖 Rumble Bot</strong>
                           <p class="message__text__bot">${botMessage}</p>
                       </div>
                   </div>`;
